e19ed3abf6de9d1383cd19fc7f2610e9d865569d
EmmaKH <eaa24ekh@students.eaaa.dk>
2025-04-29 17:10:13 +0200

    Fad og batch + associering mellem disse klasser Controller og storage til fad og batch

--- src/application/controller/Controller.java
+++ src/application/controller/Controller.java
@@ -1,4 +1,22 @@
 package application.controller;
 
+import application.model.Batch;
+import application.model.Fad;
+import application.model.Fadtype;
+import application.model.Træsort;
+import storage.Storage;
+
 public class Controller {
+
+    public static Batch createBatch(String maltBach, String kornSort, String mark, double mængdeVæske, double alkoholPct, String kommentar) {
+        Batch batch = new Batch(maltBach, kornSort, mark, mængdeVæske, alkoholPct, kommentar);
+        Storage.addBatch(batch);
+        return batch;
+    }
+
+    public static Fad createFad(int fadNr, double fadStørrelse, String levarandør, boolean erBrugt, Fadtype fadtype, Træsort træsort, int antalGangeBrugt, double literPåfyld) {
+        Fad fad = new Fad(fadNr, fadStørrelse, levarandør, erBrugt, fadtype, træsort, antalGangeBrugt, literPåfyld);
+        Storage.addFad(fad);
+        return fad;
+    }
 }
--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -1,4 +1,37 @@
 package storage;
 
+import application.model.Batch;
+import application.model.Fad;
+
+import java.util.ArrayList;
+import java.util.Base64;
+
 public class Storage {
+
+    private static ArrayList<Batch> batches = new ArrayList<>();
+    private static ArrayList<Fad> fade = new ArrayList<>();
+
+    //-----------------------------------------------------------------------------
+    public static ArrayList<Batch> getBatches() {
+        return new ArrayList<Batch>(batches);
+    }
+
+    public static void addBatch(Batch batch) {
+        batches.add(batch);
+    }
+    public static void removeBatch(Batch batch) {
+        batches.remove(batch);
+    }
+    //-----------------------------------------------------------------------------
+    public static ArrayList<Fad> getFade() {
+        return new ArrayList<Fad>(fade);
+    }
+
+    public static void addFad(Fad fad) {
+        fade.add(fad);
+    }
+    public static void removeFad(Fad fad) {
+        fade.remove(fad);
+    }
+
 }
