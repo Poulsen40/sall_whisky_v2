c5aef3b8b32711a9313aaecc99c9df4202c83e18
EmmaKH <eaa24ekh@students.eaaa.dk>
2025-05-15 14:30:33 +0200

    GUI opretwhiksy ændret

--- src/application/model/DestillatMængde.java
+++ src/application/model/DestillatMængde.java
@@ -1,16 +1,24 @@
 package application.model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class DestillatMængde implements Serializable {
     private double mængde;
     private Destillat destillat;
+    private LocalDateTime slutdato;
 
     DestillatMængde(double mængde, Whiskyserie whiskyserie, Destillat destillat) {
         this.mængde = mængde;
         this.destillat = destillat;
         whiskyserie.addDestillatMængde(this);
+        setSlutdato(LocalDateTime.now());
     }
+
+    public void setSlutdato(LocalDateTime slutdato) {
+        this.slutdato = slutdato;
+    }
+
 
     public double getMængde() {
         return mængde;
